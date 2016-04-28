a5e99950abbd895db0ce9d18fcfaebfba41a8e51
maaron <m_aaron@yahoo.com>
2016-04-28 08:15:33 -0500

    Basic expression transform and serialization sample

---
+++ Class1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using IQToolkit;
+using Serialize.Linq.Serializers;
+
+namespace WebLinq
+{
+    public static class ExpressionExtensions
+    {
+        public static bool HasArgImplementing(this MethodCallExpression expression, Type type)
+        {
+            return expression.Arguments.FirstOrDefault(a => 
+                a.Type.Implements(type)) != null;
+        }
+    }
+
+    public static class TypeExtensions
+    {
+        public static bool Implements(this Type type, Type iface)
+        {
+            var ifaceDef = iface.IsGenericType ? 
+                iface.GetGenericTypeDefinition() 
+                : iface;
+
+            return (type.IsGenericType ? type.GetGenericTypeDefinition() : type) == ifaceDef ||
+                type.GetInterfaces().FirstOrDefault(i => 
+                    (i.IsGenericType ? i.GetGenericTypeDefinition() : i) == ifaceDef)
+                != default(Type);
+        }
+    }
+
+    public class WebLinqSource<T>
+    {
+        public WebLinqProvider Provider { get; private set; }
+        public Query<T> Data { get; private set; }
+
+        public WebLinqSource(Expression<Func<IEnumerable<T>>> sourceExpression)
+        {
+            Provider = new WebLinqProvider(sourceExpression);
+            Data = new Query<T>(Provider);
+        }
+    }
+
+    public class WebLinqVisitor : System.Linq.Expressions.ExpressionVisitor
+    {
+        public Expression SourceExpression { get; private set; }
+
+        public WebLinqVisitor(Expression sourceExpression)
+        {
+            SourceExpression = sourceExpression;
+        }
+
+        private static Expression StripQuotes(Expression e)
+        {
+            while (e.NodeType == ExpressionType.Quote)
+                e = ((UnaryExpression)e).Operand;
+
+            return e;
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.HasArgImplementing(typeof(IQueryable)))
+            {
+                var newArgs = node.Arguments.Select(a => StripQuotes(base.Visit(a))).ToArray();
+
+                // TODO: This is an imperfect shortcut to find an Enumerable 
+                // method that corresponds to the given Queryable method.  
+                // Should work for most (if not all) LINQ operators, though, 
+                // which is primarily what we are after.  To do this 
+                // "correctly", i.e., the same as what the compiler does, 
+                // we'd need to effectively reproduce extension method lookup, 
+                // generic overload resolution, etc.  This is not trivial.
+                var newMethod = typeof(Enumerable).GetMethods().FirstOrDefault(
+                    m => m.Name == node.Method.Name 
+                    && newArgs.Length == m.GetParameters().Length
+                    && newArgs.Zip(m.GetParameters().Select(p => p.ParameterType), 
+                        (arg, type) => arg.Type.Implements(type.GetGenericTypeDefinition())).All(b => b == true));
+
+                if (newMethod == null)
+                    throw new Exception("Couldn't find IEnumerable<> replacement for method " + node.Method.Name);
+
+                if (newMethod.IsGenericMethod) newMethod = newMethod.MakeGenericMethod(node.Method.GetGenericArguments().Select(
+                    t => t.Implements(typeof(IQueryable<>)) ? 
+                        typeof(IEnumerable<>).MakeGenericType(t.GetGenericArguments()[0])
+                        : t).ToArray());
+
+                return Expression.Call(newMethod, newArgs);
+            }
+            else return base.VisitMethodCall(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            return node.Type.Implements(typeof(IQueryable)) ?
+                Expression.Invoke(SourceExpression)
+                : base.VisitConstant(node);
+        }
+    }
+
+    public class WebLinqProvider : QueryProvider
+    {
+        public Expression SourceExpression { get; private set; }
+
+        public WebLinqProvider(Expression sourceExpression)
+        {
+            SourceExpression = sourceExpression;
+        }
+
+        public override object Execute(Expression expression)
+        {
+            var command = GetQueryText(expression);
+
+            var deserializer = new ExpressionSerializer(new JsonSerializer());
+            var deserialized = deserializer.DeserializeText(command);
+
+            return Expression.Lambda(deserialized).Compile().DynamicInvoke();
+        }
+
+        public override string GetQueryText(Expression expression)
+        {
+            var partial = PartialEvaluator.Eval(expression);
+
+            return Serialize(new WebLinqVisitor(SourceExpression)
+                .Visit(partial));
+        }
+
+        private string Serialize(Expression expression)
+        {
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            return serializer.SerializeText(expression);
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Linq;
+using WebLinq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var source = new WebLinqSource<int>(() => new[] { 1, 2, 3, 4 , 3, 2, 1});
+
+            var query =
+                from i in source.Data
+                where i > 2
+                where i >= 3
+                select i;
+
+            var result = query.ToArray();
+            Assert.IsTrue(result.SequenceEqual(new[] { 3, 4, 3 }));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var source = new WebLinqSource<int>(() => new[] { 1, 2, 3, 4, 3, 2, 1 });
+
+            var query =
+                from i in source.Data
+                from i2 in Enumerable.Range(1, i)
+                where i2 <= i
+                select i2;
+
+            var result = query.ToArray();
+            Assert.IsTrue(result.SequenceEqual(new[] {
+                1,
+                1, 2,
+                1, 2, 3,
+                1, 2, 3, 4,
+                1, 2, 3,
+                1, 2,
+                1
+            }));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var source = new[] {
+                new WebLinqSource<int>(() => new[] { 1, 2, 3 }),
+                new WebLinqSource<int>(() => new[] { 4, 5, 6 })
+            };
+
+            var query =
+                from src in source
+                from i in src.Data
+                where i % 2 == 0
+                select i;
+
+            var result = query.ToArray();
+            Assert.IsTrue(result.SequenceEqual(new[] {
+                2, 4, 6
+            }));
+        }
+    }
+}
